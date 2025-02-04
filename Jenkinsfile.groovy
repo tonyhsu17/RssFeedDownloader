0a7738f193c9bf854f373ac75ffd89448c942245
Tony Hsu <tonyhsu17@users.noreply.github.com>
2025-02-03 22:56:41 -0500

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -11,8 +11,8 @@ node('maven') {
         def des = '/shares/unsorted-downloads/watch'
         sh "docker build -t org.tonyhsu17.rss-downloader:${version} ."
         withCredentials([string(credentialsId: 'rss-url', variable: 'RSS_URL')]) {
-            sh "docker run org.tonyhsu17.rss-downloader:${version} " +
-                    "-e RSS_URL=${RSS_URL} -e RSS_DES=${des}"
+            sh "docker run -e RSS_URL=${RSS_URL} -e RSS_DES=${des} " +
+                    "org.tonyhsu17.rss-downloader:${version}"
 //            sh script: """
 //                mvn clean compile assembly:single
 //                ls target
