d23edad97eab854608bbccfca7cf42f9da97e221
Tony Hsu <tonyhsu17@users.noreply.github.com>
2025-02-06 00:43:14 -0500

    updated long args

--- src/main/java/org/tonyhsu17/Params.java
+++ src/main/java/org/tonyhsu17/Params.java
@@ -11,10 +11,10 @@ import org.tonyhsu17.utilities.commandline.Parameter;
  *
  */
 public class Params {
-    public static final Parameter D = new Parameter("d", true, "destination path");
-    public static final Parameter U = new Parameter("u", true, "rss url");
-    public static final Parameter I = new Parameter("i", true, "cron time");
-    public static final Parameter ONCE = new Parameter("once", false, "run once, overrides cron time");
+    public static final Parameter D = new Parameter("d", "des", true, "destination path");
+    public static final Parameter U = new Parameter("u", "url", true, "rss url");
+    public static final Parameter I = new Parameter("i", "interval", true, "cron time");
+    public static final Parameter ONCE = new Parameter("once", "once", false, "run once, overrides cron time");
 
     public static Parameter[] params = {D, U, I, ONCE};
 }
