619189dbaca2e78902fab80d1c19e44f49fdfb56
Tony Hsu <tonyhsu17@users.noreply.github.com>
2025-02-06 00:13:49 -0500

    Update RssFeedDownloader.java

--- src/main/java/org/tonyhsu17/RssFeedDownloader.java
+++ src/main/java/org/tonyhsu17/RssFeedDownloader.java
@@ -77,4 +77,8 @@ public class RssFeedDownloader implements Logger {
             }
         }
     }
+
+    public static void main(String[] args) throws IOException {
+        new RssFeedDownloader(args).run();
+    }
 }
